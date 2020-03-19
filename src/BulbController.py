9f185f567f6ccddd121e7f55b9508d32cf54622f
reis278 <rafael.reis@bandtec.com.br>
2020-03-19 20:01:10 -0300

    Alterando temperatura da cor ao setar o branco

--- src/BulbController.py
+++ src/BulbController.py
@@ -4,7 +4,7 @@ import src.utils as utils
 import src.CronJob as cron
 import time
 
-lampadas = ['10.3.0.21', '10.3.0.22', '10.3.0.23']
+lampadas = ['10.3.0.21', '192.168.255.14', '10.3.0.23']
 
 environments=[{"id": 1,"nome":"Biblioteca", "lados": [{"id":100,"nome": "FRENTE", "ips": [lampadas[0]]},{"id":0,"nome": "TUDO", "ips":[lampadas[0], lampadas[1]]},{"id":200,"nome": "TRAS", "ips": [lampadas[1]]}]},{"id": 2,"nome":"1B", "lados": [{"id":100, "nome": "FRENTE", "ips": [lampadas[2]]},{"id":200, "nome": "TRAS", "ips": [lampadas[2]]}]},{"id": 0,"nome":"TUDOTUDO", "lados": [{"id":0, "nome": "TUDO", "ips": [lampadas[0], lampadas[1], lampadas[2]]}]}]
 bulbs = []
@@ -78,11 +78,13 @@ def setColor(req):
     r = req.json.get('r')
     g = req.json.get('g')
     b = req.json.get('b')
-    
+
     for bulb in listBulbs:
+        bulb.set_color_temp(4700)
         bulb.set_rgb(r, g, b)
-
-
+        if r and g and b == 255:
+            bulb.set_color_temp(8000)
+        
 def bandtecColor(req):
     listBulbs, _, _ = utils.getSide(req, environments, bulbs)
     idEnv = 0
